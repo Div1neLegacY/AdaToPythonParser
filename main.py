a9523b508bec395607944a8bffd3747acfa508df
Div1neLegacY <164106818+Div1neLegacY@users.noreply.github.com>
2026-04-29 16:23:28 -0400

    Add support for nested Ada record retrieval
    Added functionality to retrieve and print a nested Ada record.

--- main.py
+++ main.py
@@ -15,5 +15,9 @@ def main():
     ada_record = ada_parser.get_record_rep_clause("src/rep_record_clause_sample.ads", "Internal_Msg_Payload")
     print(f"Found Ada record: {ada_record}")
 
+    # Sample record #4 (Nested Record)
+    ada_record = ada_parser.get_record_rep_clause("src/rep_record_clause_sample.ads", "Internal_Msg_Payload_Nested")
+    print(f"Found Ada record: {ada_record}")
+
 if __name__ == "__main__":
     main()
